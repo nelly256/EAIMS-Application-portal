692fdf76d41eac29ed3d6d58b7e6deba1edf6e50
Mosh-git <mushabedavid2002@gmail..com>
2026-09-23 00:26:29 +0300

    fix: update CSP to allow FontAwesome and Google Fonts CDN for icons and web fonts

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ app.use(helmet({
     directives: {
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'"],
-      styleSrc: ["'self'"],
+      styleSrc: ["'self'", "https://cdnjs.cloudflare.com", "https://fonts.googleapis.com"],
       imgSrc: ["'self'", 'data:'],
       connectSrc: ["'self'"],
-      fontSrc: ["'self'"],
+      fontSrc: ["'self'", "https://cdnjs.cloudflare.com", "https://fonts.gstatic.com"],
       objectSrc: ["'none'"],
       baseUri: ["'self'"],
       formAction: ["'self'"],
